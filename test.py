6c2af486f54c4f0721fb719eb759b3982ab46c13
wumeng2264@ubuntu.com <wumeng2264@ubuntu.com>
2024-10-09 18:19:55 +0800

    实现局域网连接

--- test.py
+++ test.py
@@ -80,4 +80,4 @@ def handle_command(data):
         emit('response', {'result': 'SSH channel not established'})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True,allow_unsafe_werkzeug=True)
+    socketio.run(app,host='0.0.0.0',port=5000, debug=True,allow_unsafe_werkzeug=True)
